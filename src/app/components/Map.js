92aec03f3217b6241c3f87bf5cf2a21c5443dacc
sangeeta8642 <sangeetaperagani8642@gmail.com>
2024-08-15 00:45:46 +0530

    4th

--- src/app/components/Map.js
+++ src/app/components/Map.js
@@ -1,38 +1,45 @@
 "use client"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import L from 'leaflet'; 
 import 'leaflet/dist/leaflet.css';
 
-const Map = dynamic(() => import('react-leaflet').then((mod) => mod.MapContainer), { ssr: false });
-
-const customIcon = new L.Icon({
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-  shadowSize: [41, 41],
-});
+const MapContainer = dynamic(() => import('react-leaflet').then(mod => mod.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import('react-leaflet').then(mod => mod.TileLayer), { ssr: false });
+const Marker = dynamic(() => import('react-leaflet').then(mod => mod.Marker), { ssr: false });
+const Popup = dynamic(() => import('react-leaflet').then(mod => mod.Popup), { ssr: false });
 
 const markers = [
   { position: [28.6139, 77.209], label: "New Delhi" },
   { position: [19.076, 72.8777], label: "Mumbai" },
   { position: [13.0827, 80.2707], label: "Chennai" },
   { position: [22.5726, 88.3639], label: "Kolkata" },
-  { position: [12.9716, 77.5946], label: "Bangalore" },
-  
+  { position: [12.9716, 77.5946], label: "Bangalore" }
 ];
 
 const MapComponent = () => {
   const [map, setMap] = useState(null);
+  const [LInstance, setLInstance] = useState(null);
+
+  useEffect(() => {
+    import('leaflet').then((L) => {
+      setLInstance(L);
+    });
+  }, []);
 
   const handleMarkerClick = (position) => {
     if (map) {
       map.setView(position, map.getZoom());
     }
   };
+
+  const customIcon = LInstance ? new LInstance.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+    shadowSize: [41, 41],
+  }) : null;
 
   return (
     <MapContainer
